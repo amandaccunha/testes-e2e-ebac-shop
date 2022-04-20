9d2f9a81df3953afa48b5d4ba89c3b37f9a7d623
Amanda Cunha <amandacbcunha@gmail.com>
2022-04-20 15:04:31 -0300

    altereçãoes usando comandos customizados

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+import EnderecoPage from "../support/page_objects/endereco.page"
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     /*  Como cliente 
@@ -9,13 +10,24 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         Preenchendo todas opções no checkout
         E validando minha compra ao final */
 
-    beforeEach(() => {
-        cy.visit('/')
-    });
 
-    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
-    });
-
-
-})
+        beforeEach(() => {
+            cy.visit('produtos')
+        });
+    
+      it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta - Usando Comandos Customizados', () => {
+        cy.addProdutos('Arcadio Gym Short', '34', 'Black', 1)
+        cy.visit('produtos')
+        cy.addProdutos('Aether Gym Pant', '33', 'Blue', 1)
+        cy.visit('produtos')
+        cy.addProdutos('Abominable Hoodie', 'S', 'Red', 1)
+        cy.visit('produtos')
+        cy.addProdutos('Aero Daily Fitness Tee', 'S', 'Black', 1)
+    
+        cy.get('.woocommerce-message > .button').click()
+        cy.get('.checkout-button').click()
+    
+        EnderecoPage.editarEnderecoFaturamento('Marcia', 'Melo', 'Globo', 'Brasil', 'Av. das Américas', '4660', 'Rio De Janeiro', 'Rio De Janeiro','22640102', '21965370099', 'marcia22@gmail.com')
+        cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
+      })
+    })
